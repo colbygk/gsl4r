3d34a646dfd3adef8268a342b64d679b645cd135
Colby Gutierrez-Kraybill <colby@gks.hcro.org>
2010-02-24 14:35:56 -0800

    tool for creating wrapper declarations from C code

---
+++ bin/generate_stubs.rb
@@ -0,0 +1,63 @@
+#!/usr/bin/env ruby
+
+# This program generates attach_gsl_function stubs, based on the C prototypes
+# The format of the input file should be one, well formed prototype per line
+# e.g.
+#   gsl_complex gsl_complex_arcsin (gsl_complex a);  /* r=arcsin(a) */
+
+if ARGV.length < 1 
+  raise %Q{Must pass in at least one filename, file must have list of functions}
+end
+
+class Array
+  def type_info
+    select { |x| index(x) % 2 == 0 }
+  end
+end
+
+# Split prototype into method name, arguments, return type and comment section
+def split_prototype( prototype )
+  prototype,comment = prototype.split(/\//)
+  prototype,args = prototype.split(/\((.*)\)/)
+  argtypes = []
+  args.split(",").each { |arg|
+    type_info, varname = arg.split(" ")
+    argtypes << type_info
+  }
+  return_type,method_name = prototype.split(" ")
+  return method_name, argtypes, return_type, "/#{comment}/"
+end
+
+implsubsmap = {
+  "gsl_complex" => "GSL_Complex.by_value",
+  "double" => ":double"
+}
+
+typesubsmap = {
+  "gsl_complex" => "GSL_Complex",
+  "double" => ":double"
+}
+  
+ARGV.each do |file|
+  File.open(file, "r") do |infile|
+    while line = infile.gets
+      method_name, args, return_type, comment = split_prototype( line )
+      c_return = return_type.gsub(return_type, implsubsmap[return_type])
+      r_return = return_type.gsub(return_type, typesubsmap[return_type])
+      c_args = "[ "
+      r_args = c_args
+      args.each do |arg|
+	c_args += arg.gsub(arg, implsubsmap[arg]) + ", "
+	r_args += arg.gsub(arg, typesubsmap[arg]) + ", "
+      end
+      c_args = c_args.chop.chop + " ]"
+      r_args = r_args.chop.chop + " ]"
+
+      puts "#{comment}"
+      puts "attach_gsl_function :#{method_name}, #{c_args}, #{c_return}, #{r_args}, #{r_return}"
+    end
+  end
+end
+
+
+
