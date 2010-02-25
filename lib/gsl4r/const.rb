4f2064a8c68cebf2426aa6a8f4f78a9b29b63aa6
Colby Gutierrez-Kraybill <colby@gks.hcro.org>
2010-02-24 23:54:25 -0800

    automatic generator of consts files, and the actual consts files

---
+++ lib/gsl4r/const.rb
@@ -0,0 +1,14 @@
+#
+# == Other Info
+#
+# Author::	Colby Gutierrez-Kraybill
+# Version::	$Id$
+#
+
+
+require 'const_num.rb'
+require 'const_cgs.rb'
+require 'const_cgsm.rb'
+require 'const_mks.rb'
+require 'const_mksa.rb'
+
