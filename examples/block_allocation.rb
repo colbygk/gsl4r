322d08e361b01850f0e8ed303e2b95333a05243f
colbygk <colby@astro.berkeley.edu>
2010-03-19 16:41:33 -0700

    pretty up variable names

--- examples/block_allocation.rb
+++ examples/block_allocation.rb
@@ -1,16 +1,34 @@
+$: << File.join('..','lib')
+
 require 'rubygems'
 require 'gsl4r'
 require 'gsl4r/block'
 include FFI
-op1 = MemoryPointer.new :pointer
-op1 = GSL4r::Block::Methods::gsl_block_alloc(50)
-puts op1.get_ulong(0)
-ob1=GSL4r::Block::GSL_Block.new(op1)
-ob2=GSL4r::Block::GSL_Block_Cast.new(op1)
-#puts ob1.length
-#op1.put_array_of_double(1, (1..50).to_a.collect { |i| i=i*5.0 } )
-#ob1[:data].put_array_of_double(0, (1..50).to_a.collect { |i| i=i*5.0 } )
-puts ob2.set( (1..50).to_a.collect { |i| i=i*5.0 } )
-v = ob2.values
-v[1] = 3000.0
-puts v
+
+bp1 = MemoryPointer.new :pointer
+bp1 = GSL4r::Block::Methods::gsl_block_alloc(50)
+
+# Creating object wrapper around pointer
+blk1=GSL4r::Block::GSL_Block.new(bp1)
+
+puts "blk1.length: #{blk1.length}"
+
+# Creating a 'cast' object around pointer
+blk2=GSL4r::Block::GSL_Block_Cast.new(bp1)
+
+puts "blk2.length: #{blk2.length}"
+
+# assign values to castable object
+blk2.set( (1..50).to_a.collect { |i| i=i*5.0 } )
+
+# get those values and show them
+v = blk2.values
+printf "blk2.values: "
+v.each { |i| printf "%g ", i }
+
+# show that the non-cast object is referencing bp1
+z = blk1.values
+printf "\nblk1.values: "
+z.each { |i| printf "%g ", i }
+
+printf "\n"
